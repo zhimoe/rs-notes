cfb97511487dedce88f128f7733b59306fe979fb
cod3fn <xiongwengui67@gmail.com>
2020-06-22 07:34:11 +0800

    some notes on rust

--- src/bin/byteorder.rs
+++ src/bin/byteorder.rs
@@ -1,4 +1,6 @@
 use encoding_rs::*;
+use regex::Regex;
+use std::collections::HashMap;
 
 fn main() {
     use byteorder::{BigEndian, LittleEndian, ReadBytesExt};
@@ -11,8 +13,27 @@ fn main() {
     println!("{}", num1);//2345806337
 
 
-    let header: &[u8] = b"<\x00D\x00i\x00c\x00t\x00i\x00o\x00n\x00a\x00r\x00y\x00 \x00G\x00e\x00n\x00e\x00r\x00a\x00t\x00e\x00d\x00B\x00y\x00E\x00n\x00g\x00i";
-    let mut decoder = UTF_16LE.new_decoder();
-    let &mut s;
-    decoder.decode_to_str(&header, &mut s, false);
+    let s = r##"<Dictionary GeneratedByEngineVersion="2.0" RequiredEngineVersion="2.0" Format="Html" KeyCaseSensitive="No" StripKey="Yes" Encrypted="2"
+    RegisterBy="EMail"
+    Description="&lt;table width=&quot;100%&quot; border=&quot;0&quot; bgcolor=&quot;#fef4f4&quot;&gt;&lt;tr&gt;&lt;td align=&quot;center&quot;&gt;&lt;h1 style=&quot;color:deeppink; font-family: Microsoft Yahei, sans-serif; margin-top: 5pt&quot;&gt;朗文当代高级英语辞典（第四版）&lt;/h1&gt;&lt;/td&gt;&lt;/tr&gt;&lt;tr&gt;&lt;td align=&quot;center&quot;&gt;&lt;h3 style=&quot;color:#e95295; font-family: Microsoft Yahei, sans-serif&quot;&gt;以Oeasy提供的APK为底稿，水湲2014年6月25日制作，2015年7月19日更新&lt;/h3&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;"
+    Title="朗文当代双解"
+    Encoding="UTF-8"
+    CreationDate="2015-7-18"
+    Compact="Yes"
+    Compat="Yes"
+    Left2Right="Yes"
+    DataSourceFormat="106"
+    StyleSheet=""/>"##;
+    let mut _header_map = HashMap::new();
+    let re = Regex::new(r#"(\w+)=["](.*?)["]"#).unwrap();
+    let caps = re.captures_iter(s);
+    let m1 = re.find_iter(s);
+    for cap in caps {
+        _header_map.insert(cap.get(1).unwrap().as_str(), cap.get(2).unwrap().as_str());
+        // println!("{}", cap.get(1).unwrap().as_str());
+        // println!("{}", cap.get(2).unwrap().as_str());
+    }
+    for kv in _header_map{
+        println!("{}={}", kv.0, kv.1);
+    }
 }
--- src/bin/mdx.rs
+++ src/bin/mdx.rs
@@ -5,11 +5,60 @@ use std::fs::File;
 use adler32::RollingAdler32;
 
 use regex::Regex;
-use byteorder::LittleEndian;
+use byteorder::{LittleEndian, ReadBytesExt, BigEndian};
+use std::char::decode_utf16;
+use std::collections::HashMap;
+
+
+struct HeaderInfo {
+    generatedbyengineversion: f32,
+    requiredengineversion: f32,
+    format: String,
+    keycasesensitive: bool,
+    stripkey: bool,
+    /**
+     * encryption flag
+     * 0x00 - no encryption
+     * 0x01 - encrypt record block
+     * 0x02 - encrypt key info block
+     */
+    encrypted: bool,
+    registerby: String,
+    description: String,
+    title: String,
+    encoding: String,
+    creationdate: String,
+    compact: bool,
+    left2right: bool,
+    datasourceformat: String,
+    stylesheet: String,
+}
+
+impl Default for HeaderInfo {
+    fn default() -> Self {
+        HeaderInfo {
+            generatedbyengineversion: 0.0,
+            requiredengineversion: 0.0,
+            format: "".to_string(),
+            keycasesensitive: false,
+            stripkey: false,
+            encrypted: false,
+            registerby: "".to_string(),
+            description: "".to_string(),
+            title: "".to_string(),
+            encoding: "".to_string(),
+            creationdate: "".to_string(),
+            compact: false,
+            left2right: false,
+            datasourceformat: "".to_string(),
+            stylesheet: "".to_string(),
+        }
+    }
+}
 
 struct KeyInfo {
-    tailerKeyText: Vec<u8>,
-    headerKeyText: Vec<u8>,
+    tail_key_text: Vec<u8>,
+    header_key_text: Vec<u8>,
 
     key_block_compressed_size_accumulator: u128,
     key_block_compressed_size: u128,
@@ -20,19 +69,10 @@ struct KeyInfo {
 }
 
 struct Mdx {
-    fname: String,
-    //file path
-    isCompact: bool,
-    isStripKey: bool,
-    isKeyCaseSensitive: bool,
-    emptyStr: String,
-    /**
-     * encryption flag
-     * 0x00 - no encryption
-     * 0x01 - encrypt record block
-     * 0x02 - encrypt key info block
-     */
-    _encrypt: i16,
+    filename: String,
+    header_info: HeaderInfo,
+
+    empty_str: String,
     _charset: String,
     _encoding: String,
     delimiter_width: i8,
@@ -43,10 +83,10 @@ struct Mdx {
     _num_key_blocks: u128,
     _num_record_blocks: u128,
 
-    accumulation_blockId_tree: RBTree<i32, String>,
+    accumulation_block_id_tree: RBTree<i32, String>,
     _key_block_size: u128,
     _key_block_info_size: u128,
-    _key_block_info_decomp_size: u128,
+    _key_block_info_decom_size: u128,
     _record_block_size: u128,
     _record_block_offset: u128,
     _record_block_start: u128,
@@ -54,35 +94,102 @@ struct Mdx {
     _key_block_info_list: Vec<KeyInfo>,
     _record_info_struct_list: Vec<u8>,
 
-    maxComRecSize: i32,
-    maxDecompressedSize: u128,
+    max_com_rec_size: i32,
+    max_decompressed_size: u128,
     rec_decompressed_size: i32,
 
-    maxDecomKeyBlockSize: u128,
-    maxComKeyBlockSize: u128,
+    max_decom_key_block_size: u128,
+    max_com_key_block_size: u128,
 
 }
 
-// 等价下面的代码
+impl Default for Mdx {
+    fn default() -> Mdx {
+        Mdx {
+            filename: "".to_string(),
+            header_info: HeaderInfo {
+                generatedbyengineversion: 0.0,
+                requiredengineversion: 0.0,
+                format: "".to_string(),
+                keycasesensitive: false,
+                stripkey: false,
+                encrypted: false,
+                registerby: "".to_string(),
+                description: "".to_string(),
+                title: "".to_string(),
+                encoding: "".to_string(),
+                creationdate: "".to_string(),
+                compact: false,
+                left2right: false,
+                datasourceformat: "".to_string(),
+                stylesheet: "".to_string(),
+            },
+            empty_str: "".to_string(),
+            _charset: "".to_string(),
+            _encoding: "".to_string(),
+            delimiter_width: 0,
+            _passcode: "".to_string(),
+            _version: 0.0,
+            _number_width: 0,
+            _num_entries: 0,
+            _num_key_blocks: 0,
+            _num_record_blocks: 0,
+            accumulation_block_id_tree: RBTree::new(),
+            _key_block_size: 0,
+            _key_block_info_size: 0,
+            _key_block_info_decom_size: 0,
+            _record_block_size: 0,
+            _record_block_offset: 0,
+            _record_block_start: 0,
+            _key_block_offset: 0,
+            _key_block_info_list: vec![],
+            _record_info_struct_list: vec![],
+            max_com_rec_size: 0,
+            max_decompressed_size: 0,
+            rec_decompressed_size: 0,
+            max_decom_key_block_size: 0,
+            max_com_key_block_size: 0,
+        }
+    }
+}
+
+// big endian bytes unpack,等价下面的代码
 // from struct import unpack
-// unpack unpack('>I', bytes_arr)
+// unpack('>I', bytes_arr)
 //  b"\x00\x00\x06V" => 1622
-fn big_endian_bytes_unpack(byte: &[u8]) -> u32 {
-    use byteorder::{BigEndian, ReadBytesExt};
+fn be_bytes_unpack(byte: &[u8]) -> u32 {
     let mut buf_out: &[u8] = byte.clone();
     let num = buf_out.read_u32::<BigEndian>().unwrap();
     num
 }
 
-// unpack unpack('<I', bytes_arr)
+// unpack('<I', bytes_arr)
 // b"\x01*\xd2\x8b" => 2345806337
-fn little_endian_bytes_unpack(byte: &[u8]) -> u32 {
-    use byteorder::{LittleEndian, ReadBytesExt};
+fn le_bytes_unpack(byte: &[u8]) -> u32 {
     let mut buf_out: &[u8] = byte.clone();
     let num = buf_out.read_u32::<LittleEndian>().unwrap();
     num
 }
 
+// // python bytes.decode("utf-16").encode("utf-8")
+// fn u8_to_utf16_string(bytes: &[u8]) -> Option<String> {
+//     let (front, slice, back) = unsafe {
+//         bytes.align_to::<u16>()
+//     };
+//     if front.is_empty() && back.is_empty() {
+//         String::from_utf16(slice).ok()
+//     } else {
+//         None
+//     }
+// }
+// 等价上面的u8_to_utf16_string方法
+pub fn le_u8_string(slice: &[u8]) -> Option<String> {
+    let idx = slice.len() / 2;
+    let iter = (0..idx)
+        .map(|i| u16::from_le_bytes([slice[2 * i], slice[2 * i + 1]]));
+
+    std::char::decode_utf16(iter).collect::<Result<String, _>>().ok()
+}
 
 impl Mdx {
     fn new(f: String) -> Self {
@@ -90,16 +197,17 @@ impl Mdx {
         let mut bytes1 = [0; 4];
         // read exactly 4 bytes
         reader.read_exact(&mut bytes1);
-        let header_len = big_endian_bytes_unpack(&bytes1);
+        let header_len = be_bytes_unpack(&bytes1);
         let _key_block_offset: i32 = (4 + header_len + 4) as i32;
         let mut header_bytes = vec![0; header_len as usize];
 
+        // read the header info
         reader.read_exact(&mut header_bytes);
 
-        // 4 bytes: adler32 checksum of header, in little endian
+        // reade 4 bytes: adler32 checksum of header, in little endian
         let mut adler32_bytes = [0; 4];
         reader.read_exact(&mut adler32_bytes);
-        let adler32 = little_endian_bytes_unpack(&adler32_bytes);
+        let adler32 = le_bytes_unpack(&adler32_bytes);
 
 
         let mut rolling_adler32 = RollingAdler32::new();
@@ -110,50 +218,67 @@ impl Mdx {
             panic!("unrecognized format");
         }
 
+
         // header text in utf-16 encoding ending with '\x00\x00'
-        let (header_left, end) = header_bytes.split_at(header_bytes.len() - 2);
+        let (header, end) = header_bytes.split_at(header_bytes.len() - 2);
+        let header_txt = le_u8_string(&header).unwrap();
 
-        let re = Regex::new(r####"(\w+)=["](.*?)["]"####).unwrap();
-        let hd_string = String::from_utf8_lossy(&header_bytes);
-        println!("header string={}", hd_string);
+        println!("{}", header_txt);
 
-        Mdx {
-            fname: f.clone(),
-            isCompact: false,
-            isStripKey: false,
-            isKeyCaseSensitive: false,
-            emptyStr: "".to_string(),
-            _encrypt: 0,
-            _charset: "".to_string(),
-            _encoding: "".to_string(),
-            delimiter_width: 0,
-            _passcode: "".to_string(),
-            _version: 0.0,
-            _number_width: 0,
-            _num_entries: 0,
-            _num_key_blocks: 0,
-            _num_record_blocks: 0,
-            accumulation_blockId_tree: RBTree::new(),
-            _key_block_size: 0,
-            _key_block_info_size: 0,
-            _key_block_info_decomp_size: 0,
-            _record_block_size: 0,
-            _record_block_offset: 0,
-            _record_block_start: 0,
-            _key_block_offset,
-            _key_block_info_list: vec![],
-            _record_info_struct_list: vec![],
-            maxComRecSize: 0,
-            maxDecompressedSize: 0,
-            rec_decompressed_size: 0,
-            maxDecomKeyBlockSize: 0,
-            maxComKeyBlockSize: 0,
+        let mut _header_map = HashMap::new();
+        let re = Regex::new(r#"(\w+)=["](.*?)["]"#).unwrap();
+        let caps = re.captures_iter(header_txt.as_str());
+        for cap in caps {
+            _header_map.insert(cap.get(1).unwrap().as_str(), cap.get(2).unwrap().as_str());
         }
+        // for kv in _header_map {
+        //     println!("{}={}", kv.0, kv.1);
+        // }
+
+        let _version = _header_map.get("GeneratedByEngineVersion").unwrap().parse::<f32>().unwrap();
+        let headerinfo = HeaderInfo::default();
+        // for (k, v) in &_header_map.iter() {
+        //     match *k {
+        //         "GeneratedByEngineVersion" => headerinfo.generatedbyengineversion = *_version,
+        //         _ => {}
+        //     }
+        // }
+
+
+        //
+        // key block info
+        let _number_width = if _version < 2.0 { 4 } else { 8 };
+        let num_bytes = if _version < 2.0 { 4 * 4 } else { 8 * 5 + 4 };
+        let mut num_bytes_buf = vec![0; num_bytes];
+        reader.read_exact(&mut num_bytes_buf);
+
+        let (mut key_block, _tail1) = num_bytes_buf.split_at(_number_width);
+        let _num_key_blocks = key_block.read_u64::<BigEndian>().unwrap();
+        println!("_num_key_blocks={}", _num_key_blocks);
+
+        let (mut entries, _tail2) = _tail1.split_at(_number_width);
+        let _num_entries = entries.read_u64::<BigEndian>().unwrap();
+        println!("_num_entries={}", _num_entries);
+        if _version >= 2.0 {
+            let (mut key_block_info_decomp_size, _tail3) = _tail2.split_at(_number_width);
+            let _key_block_info_decomp_size = key_block_info_decomp_size.read_u64::<BigEndian>().unwrap();
+            println!("_key_block_info_decomp_size={}", _key_block_info_decomp_size);
+        }
+
+        // _num_entries = _read_number(sf);                                          // 2
+        // if (_version >= 2.0) {
+        //     _key_block_info_decomp_size = _read_number(sf);
+        // }      //[3]
+        // _key_block_info_size = _read_number(sf);                                  // 4
+        // _key_block_size = _read_number(sf);                                       // 5
+
+
+        Mdx { filename: f, header_info: headerinfo, ..Default::default() }
     }
 }
 
 fn main() {
     let mdx = Mdx::new(String::from("/home/cod3fn/code/rs-notes/resources/LSC4.mdx"));
     println!("mdx version {}", mdx._version);
-    println!("mdx version {}", mdx.fname);
+    println!("mdx filename {}", mdx.filename);
 }
